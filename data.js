c7b8358b421e0bfaf19f33a889045ee03e407a74
1620432349 <1620432349@qq.com>
2026-07-15 16:03:58 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1298,7 +1298,7 @@ window.STATIC_DATA = {
     },
     {
       "id": 87,
-      "title": "��0721-0804�������Ϻ���·�������Σ����ţ����޹�˾",
+      "title": "0721-0804东北（上海车）",
       "category": "供应商合同",
       "subsidiary": "遇见美好",
       "supplier": "上海铁路国际旅游（集团）有限公司",
@@ -1312,7 +1312,7 @@ window.STATIC_DATA = {
       "fileType": ".pdf",
       "fileSize": 6729889,
       "createdAt": "2026-07-09T05:36:29.000Z",
-      "updatedAt": "2026-07-15T07:58:02.899Z"
+      "updatedAt": "2026-07-15T08:03:36.990Z"
     },
     {
       "id": 88,
